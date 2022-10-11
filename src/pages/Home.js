a5f14fd85a506e7f2eb403f22b503a77af9ce82c
Pauline Ondiek <paulineondiek76@gmail.com>
2022-10-11 07:57:31 -0400

    styling the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,36 @@ import Nav from "../components/Nav"
 import styled from "styled-components"
 
 const HomeContainer = styled.div`
-
 `
+const SelectContainer=styled.div`
+display:flex;
+flex-direction:column;
+gap:2em;
+align-items:center;`
 const Container=styled.div`
 margin:0 auto;
 width:80%;`
-const HomeHead=styled.h2``
+const HomeHead=styled.h2`
+color:#759F82;`
 const HomePara=styled.p``
 const SelectOption=styled.select`
 display:flex;
+width:40%;
 flex-direction:column;
-align-items:center;
+outline:none;
+border:none;
+border:1px solid #759F82;
+border-radius:10px;
+padding:1em .5em;
+`
+const Button=styled.button`
+padding:1em .5em;
+background:#C8DBBE;
+border-radius:5px;
+border:none;
+cursor:pointer;
+color:white;
+
 `
 
 
@@ -24,9 +43,11 @@ const Home = () => {
             
             <Nav/>
             <Container>
+            <SelectContainer>
             <HomeHead>Check Your Typing Skills</HomeHead>
             <HomePara>Select your Test</HomePara>
-            <SelectOption>
+           
+                <SelectOption>
                 <option value="">Please select time</option>
                 <option>1 minute</option>
                 <option>2 minutes</option>
@@ -38,8 +59,10 @@ const Home = () => {
                 <option>Easy</option>
                 <option>Medium</option>
                 <option>Hard</option>
-                
+                 
             </SelectOption>
+            <Button>Start Test</Button>
+            </SelectContainer>
             </Container>
         </HomeContainer>
     )
