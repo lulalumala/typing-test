b48203008c6c4ae48fa0c485aabe8ae0b6739da2
Pauline Ondiek <paulineondiek76@gmail.com>
2022-10-11 07:11:36 -0400

    started working on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,45 @@ import Nav from "../components/Nav"
 import styled from "styled-components"
 
 const HomeContainer = styled.div`
+
 `
+const Container=styled.div`
+margin:0 auto;
+width:80%;`
+const HomeHead=styled.h2``
+const HomePara=styled.p``
+const SelectOption=styled.select`
+display:flex;
+flex-direction:column;
+align-items:center;
+`
+
+
 
 const Home = () => {
 
     return (
         <HomeContainer>
+            
             <Nav/>
+            <Container>
+            <HomeHead>Check Your Typing Skills</HomeHead>
+            <HomePara>Select your Test</HomePara>
+            <SelectOption>
+                <option value="">Please select time</option>
+                <option>1 minute</option>
+                <option>2 minutes</option>
+                <option>3 minutes</option>
+            
+            </SelectOption>
+            <SelectOption>
+                <option>Select Level</option>
+                <option>Easy</option>
+                <option>Medium</option>
+                <option>Hard</option>
+                
+            </SelectOption>
+            </Container>
         </HomeContainer>
     )
 }
