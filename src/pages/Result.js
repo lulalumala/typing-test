113e8a5cf8ec24546728746d0c5587182c0282a3
lulalumala <stephenlumala@gmail.com>
2022-10-12 08:54:39 +0300

    pushed to receive the updated code

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -12,7 +12,8 @@ justify-content: center;
 const ResultTest = styled.p`font-size: 3rem;
 `
 const P = styled.p`font-size: 2rem;`
-const Div = styled.div`box-shadow: 0 0 5px gray;
+const Div = styled.div`
+box-shadow: 0 0 5px gray;
 border-radius: 50%;
 width: 200px;
 height: 200px;
@@ -21,13 +22,15 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 background:#EDE4E0;`
-const Button = styled.button`padding: 1em 2em;
+const Button = styled.button`
+padding: 1em 2em;
 border-radius: 5px;
 cursor: pointer;
 font-size: 1.5rem;
 margin: 0 auto;
 background:#EDE4E0;`
-const ButtonContainer = styled.div`display: flex;
+const ButtonContainer = styled.div`
+display: flex;
 margin: 1em 0;`
 const Result = () => {
 
--- src/pages/Typing.js
+++ src/pages/Typing.js
@@ -6,16 +6,17 @@ background:#759F82;
 height:100vh;
 `
 
-const Input = styled.input`width: 100%;
-font-size: 3rem;
-padding: .5em 0;
-border: none;
-outline: none;
-border-bottom: 1px solid black;
+const Textarea = styled.textarea`
 background: inherit;
+width: 100%;
+outline: none;
+padding: .5em 0;
+font-size: 2rem;
+margin-top: 1em;
 `
 
-const Div = styled.div``
+const Div = styled.div`
+`
 const P = styled.p`font-size:2.5rem;`
 const TypingArea = styled.div`
 width: 80%;
@@ -33,12 +34,11 @@ const Typing = () => {
 
             <TypingContainer>
                 <TypingArea>
-                    <Div><P>The quick brown fox jumps over a lazy dog. He stores golden fish in aquariums
+                    <Div><P>The quick brown fox jumps over a lazy dog. He stores golden fish in aquariums.
                     </P>
                         <P>Tongue twister! Kantai can tie a tie, why can't I tie a tie like Kantai tie a tie? So social shosho saw a source show. does she sells sea shells at the sea shore? She sells sea shells at the seashore</P></Div>
-                    <Input type="text" />
-                    <Input type="text" />
-                    <Input type="text" />
+                    <Textarea></Textarea>
+
                 </TypingArea>
             </TypingContainer>
         </>
