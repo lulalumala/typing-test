f559780c9902885c0c90357d53157e057adfa447
Pauline Ondiek <paulineondiek76@gmail.com>
2022-10-14 05:08:02 -0400

    modals

--- src/pages/Modals.js
+++ src/pages/Modals.js
@@ -17,12 +17,11 @@ const Modals=()=>{
     }
     return(
         <>
-        <h1>Hello World</h1>
 
         {
             show &&//short circuit
         <div style={{width:"300px", height:"300px", background:"red", position:"absolute", top:"50%", left:"50%", transform:"translate(-50%,-50%)"}}>
-            <p>This is the Modal</p>
+             <p>This is the Modal</p>
             <Button onClick={()=>setShow(false)}>Close</Button> 
         </div>
         }
