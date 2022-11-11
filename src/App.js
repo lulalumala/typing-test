4f8903c7b2436cf7f0f689d0f4687c24ddd752b4
lulalumala <stephenlumala@gmail.com>
2022-11-11 11:55:07 +0300

    created a separate div as component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Login from "./pages/Login";
 import Result from "./pages/Result";
 import Typing from "./pages/Typing";
 import Signup from "./pages/Signup";
-import Modals from "./components/Modals";
 
 function App() {
   const [timeSelected, setTimeSelected] = useState(0)
+  const [startTimer, setStartTimer] = useState(false)
 
   const [show, setShow]=useState(false)
   return (
@@ -17,10 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home timeSelected={ timeSelected } setTimeSelected={setTimeSelected} />}></Route>
         <Route path="/login" element={<Login/>} ></Route>
-        <Route path="/typing" element={<Typing timeSelected={timeSelected} show={show} setShow={setShow}/>}></Route>
+        <Route path="/typing" element={<Typing timeSelected={timeSelected} show={show} setShow={setShow} startTimer={startTimer} setStartTimer={setStartTimer} />}></Route>
         <Route path="/result" element={<Result/>} ></Route>
         <Route path="/signup" element={<Signup/>} ></Route>
-        <Route path="/modals" element={<Modals />} ></Route>
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Typing.js
+++ src/pages/Typing.js
@@ -1,33 +1,16 @@
 import Nav from "../components/Nav";
 import styled from "styled-components";
-import { useRef, useState, useEffect } from "react";
-import Timer from "../components/Timer";
+import { useState } from "react";
 import Modals from "../components/Modals";
+import InputDiv from "../components/InputDiv";
 
 
 const TypingContainer = styled.div`
 background:#759F82;
 height:100vh;
+font-size:1.5rem;
+padding: 1.4em;
 `
-
-const TextDiv = styled.div`
-background: inherit;
-border: 1px solid black;
-width: 100%;
-outline: none;
-padding: .5em 0;
-font-size: 2rem;
-margin-top: 1em;
-`
-const TimerDiv = styled.div`
-position: absolute;
-right: 0;
-top: 0;
-font-size: 1.5rem;
-width: fit-content;
-border: 1px solid black;
-`
-
 const Div = styled.div`
 font-size: 2rem;
 `
@@ -54,45 +37,7 @@ width: 100vw;
 height: 100vh;
 `
 
-const Typing = ({ timeSelected, show, setShow, timeLeft, setTimeLeft }) => {
-    useEffect(() => {
-        textDivRef.current.focus()
-        // const timer = () => {
-        //     if (timeLeft.s <= 0) {
-        //         setTimeLeft(prev => ({ ...prev, m: prev.m - 1 }))
-        //         setTimeLeft({ ...timeLeft, s: 60 })
-        //         setTimeLeft(prev => ({ ...prev, s: prev.s - 1 }))
-        //     }
-
-        // }
-        // setInterval(timer, 1000)
-    }, [])
-
-
-
-    // const [timeLeft, setTimeLeft] = useState({
-    //     m: timeSelected,
-    //     s: 0,
-
-    // })
-    // useEffect(() => {
-
-    //     const timer = () => {
-    //         if (timeLeft.s <= 0) {
-    //             setTimeLeft(prev => ({ ...prev, m: prev.m - 1 }))
-    //             setTimeLeft({ ...timeLeft, s: 60 })
-    //             setTimeLeft(prev => ({ ...prev, s: prev.s - 1 }))
-
-    //         }
-
-    //     }
-    //     console.log(`${timeLeft.m}:${timeLeft.s}`)
-    //     const intervalId = setInterval(timer, 1000)
-
-    // }, [timeLeft])
-
-    const textDivRef = useRef("")
-
+const Typing = ({ timeSelected, show, setShow, timeLeft, setTimeLeft, startTimer, setStartTimer }) => {
     const [states, setStates] = useState({
         startTime: 0,
         elapsedTime: "",
@@ -103,11 +48,6 @@ const Typing = ({ timeSelected, show, setShow, timeLeft, setTimeLeft }) => {
         correctChars: [],
         originalText: []
     })
-
-
-    const [dataTyped, setDataTyped] = useState("")
-
-
 
     const words = [
         "Everybody lies. Cops lie. Lawyers lie. Witnesses lie. The victim lie. A trial is a contest of lies. And everybody on the courtroom knows this. The judge knows this. Even the jury knows this. They come into the buildings knowing they will be lied to. They take their seats in the box and agree to be lied to. The trick if you are sitting at the defense table is to be patient. To wait. Not for just any lie. But for the one you can grab on to and forge like hot iron into a sharpened blade. You then use that blade to rip the case open and spill its guts out on the floor. That’s my job, to forge the blade. To sharpen it. To use it without mercy or conscience. To be the truth in a place where everybody lies.",
@@ -121,75 +61,23 @@ const Typing = ({ timeSelected, show, setShow, timeLeft, setTimeLeft }) => {
     const randomWord = words[Math.floor(Math.random() * words.length)]
     const text = randomWord.split(" ")
 
-    const onChangeFunction = (e) => {
-        const inputValue = e.target.innerText
-        if (inputValue.length === randomWord.length) {
-            setStates(prev => ({ ...prev, typedWords: [...prev.typedWords, inputValue] }))
-            setStates(prev => ({ ...prev, originalText: [...prev.originalText, text.join(" ")] }))
-            textDivRef.current.innerText = ""
-
-        }
-        // decreasing timer
-
-
-        let wpm = randomWord.split("").length / timeSelected.m
-
-
-
-
-        console.log(states)
-
-        let correct = []
-        let incorrect = []
-
-
-        // check errors and correct elements
-        for (let char of inputValue) {
-
-            const indexOfChar = randomWord.indexOf(char)
-            randomWord.charAt(indexOfChar) === char ? correct.push("correct") : incorrect.push("incorrect")
-
-
-            // setting correct and incorrect states
-            // setStates(prev => ({ ...prev, correctChars: [...prev.correctChars, "correct"] })) : setStates(prev => ({ ...prev, incorrectChars: [...prev.incorrectChars, "incorrect"] }))
-
-            //    console.log(`The index of ${char} is ${indexOfChar}`)  
-            console.log(inputValue.length)
-            //          
-        }
-        console.log(correct)
-        console.log(incorrect)
-        console.log(setShow)
-    }
-
-
-
-
-
-
-
-
     return (
 
         <TypingDiv>
-            {show && 
-            <ModalDiv >
-                <Modals setShow={setShow}/>
-            </ModalDiv>
-        }
+            {show &&
+                <ModalDiv >
+                    <Modals setShow={setShow} />
+                </ModalDiv>
+            }
             <Nav />
             <TypingContainer>
 
                 <TypingArea>
                     <Div>{randomWord} </Div>
                     <ContentDiv>
-                        <TimerDiv>
-                            <Timer timeSelected={timeSelected} show={show} setShow={setShow} />
-                        </TimerDiv>
-                        {/* <TimerDiv>{timeLeft.m}<span>m</span>:{timeLeft.s}<span>s</span></TimerDiv> */}
-                        <TextDiv contentEditable onInput={(e) => onChangeFunction(e)} ref={textDivRef} defaultValue={textDivRef.current.value} >
-                        </TextDiv>
+                        <InputDiv states={states} setStates={setStates} timeSelected={timeSelected} show={show} setShow={setShow} startTimer={startTimer} setStartTimer={setStartTimer} randomWord={randomWord} text={text} />
                     </ContentDiv>
+          {console.log(text)}
                 </TypingArea>
             </TypingContainer>
         </TypingDiv>
